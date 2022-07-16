a790718a6a585156e185ecea67841980727d6795
LucaDP07 <luke07@hotmail.it>
2022-07-16 15:56:50 +0000

    create list for variable adj3,noun3,noun12,adj11 and verb14

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import time
+#import random
 
 # Welcome messages
 
@@ -24,40 +25,49 @@ else:
 
 # User inputs story #1
 
-colors = ['red', 'blue', 'green']
+color = ['red', 'blue', 'green']
 clothing = ['socks', 'pajamas', 'skirt']
-noun = input("Enter your name: ")
-number = input("Enter your age: ")
-while not number.isnumeric():
-        number = input("Enter your age, should be a number: ")
-noun4 = input("Enter the name of a friend: ")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+while not age.isnumeric():
+        age = input("Enter your age, should be a number: ")
+
 adj = input("Enter an adjective: ")
 print ('Select number of color')
-for i,colour in enumerate(colors):
-    print(i+1, colour)
+
+for i,colors in enumerate(color):
+    print(i+1, colors)
 chosen=input('')
-while not chosen.isnumeric() or int(chosen)>len(colors):
+while not chosen.isnumeric() or int(chosen)>len(color):
         chosen = input("invalid data: ")
-color=colors[int(chosen)-1]
-noun2 = input("Enter a noun: ")
+colour=color[int(chosen)-1]
+
+noun = input("Enter a noun: ")
 verb = input("Enter a verb ending in 'ing': ")
+
 while not verb.endswith('ing'):
-        verb = input("invalid: ")
+        verb = input("invalid data: ")
+
 print ('Select your clothing')
+
 for i,clothes in enumerate(clothing):
     print(i+1, clothes)
 chosen=input('')
 while not chosen.isnumeric() or int(chosen)>len(clothing):
         chosen = input("invalid data: ")
 clothings=clothing[int(chosen)-1]
+
 adj2 = input("Enter an adjective: ")
 celebrity = input("Enter the name of a celebrity: ")
-number2 = input("Enter a number: ")
-while not number2.isnumeric():
-        number2 = input("Enter a number, should be a number: ")
-noun4 = input("Enter the name of a friend: ")
-noun5 = input("Enter a noun: ")
-noun6= input("Enter the name of another friend: ")
+number = input("Enter a number: ")
+
+while not number.isnumeric():
+        number = input("Enter a number, should be a number: ")
+
+friend = input("Enter the name of a friend: ")
+noun2 = input("Enter a noun: ")
+friend2 = input("Enter the name of another friend: ")
 time.sleep(1)
 
 # Print story #1
@@ -65,13 +75,13 @@ time.sleep(1)
 print('\nTitle: If I were president.')
 print('\n')
 time.sleep(3)
-print("My name is " + noun + "and I'm " + number + " years old.")
+print("My name is " + name + " and I'm " + age + " years old.")
 time.sleep(3)
 print("If I were president, I'd do a whole bunch of " + adj + " things:")
 time.sleep(3)
-print("I would drive the biggest " + color + " car in the country, and that")
+print("I would drive the biggest" + color + "car in the country, and that")
 time.sleep(3)
-print("car would go faster than any other " + noun2 + " in the world!")
+print("car would go faster than any other " + noun + " in the world!")
 time.sleep(3)
 print("Everyone would eat pepperoni pizza for dinner.")
 time.sleep(3)
@@ -79,12 +89,12 @@ print("I would live in the Statue of Liberty and build a " + verb + " pool at he
 time.sleep(3)
 print("I would wear a/an " + clothing + " on my head, and everyone would say I look " + adj2 + " like " + celebrity + ".")
 time.sleep(3)
-print("School would be open only " + number2 + " days a year.")
+print("School would be open only " + number + " days a year.")
 time.sleep(3)
 print("I would give my friends the best jobs.")
 time.sleep(3)
-print("I would nominate " + noun4 + " to be secretary of the " + noun5 + ",")
-print("and " + noun6 + " could be vice president !")
+print("I would nominate " + friend + " to be secretary of the " + noun2 + ",")
+print("and " + friend2 + " could be vice president !")
 time.sleep(3)
 play = input("Play again?")
 
@@ -108,24 +118,32 @@ else:
 
 # User inputs story #2
 
-verb = input("Enter a verb ending in 'ing': ")
-noun = input("Enter the name of a place: ")
-adj = input("Enter an adjective: ")
-adj2 = input("Enter an adjective: ")
-verb2 = input("Enter a verb: ")
-noun2 = input("Enter a plural noun: ")
-noun3 = input("Enter a noun: ")
-verb3 = input("Enter a verb ending in 'ing': ")
-verb4 = input("Enter a verb ending in 'ing': ")
-noun4 = input("Enter a plural noun: ")
-verb5 = input("Enter a verb ending in 'ing': ")
+adj3 = ['beautiful', 'breakable', 'meaningless']
+noun3 = ['peak', 'jungle', 'beach']
+verb2 = input("Enter a verb ending in 'ing': ")
+noun4 = input("Enter the name of a place: ")
+adj4 = input("Enter an adjective: ")
+print ('Select an adjective')
+for i,adjective in enumerate(adj2):
+    print(i+1, adjective)
+chosen=input('')
+while not chosen.isnumeric() or int(chosen)>len(adj2):
+        chosen = input("invalid data: ")
+adjective=adj2[int(chosen)-1]
+verb3 = input("Enter a verb: ")
 noun5 = input("Enter a plural noun: ")
-noun6 = input("Enter a plural noun/places: ")
+noun6 = input("Enter a noun: ")
+verb4 = input("Enter a verb ending in 'ing': ")
+verb5 = input("Enter a verb ending in 'ing': ")
+noun7 = input("Enter a plural noun: ")
 verb6 = input("Enter a verb ending in 'ing': ")
-noun7 = input("Enter a plural noun/places: ")
-adj3 = input("Enter an adjective: ")
 noun8 = input("Enter a plural noun: ")
-verb7 = input("Enter a verb: ")
+noun9 = input("Enter a plural noun/places: ")
+verb7 = input("Enter a verb ending in 'ing': ")
+noun10 = input("Enter a plural noun/places: ")
+adj5 = input("Enter an adjective: ")
+noun11 = input("Enter a plural noun: ")
+verb8 = input("Enter a verb: ")
 
 # Print story #2
 
@@ -163,24 +181,25 @@ else:
 
 # User inputs story #3
 
-
-noun = input("Enter the name of a month ")
-verb = input("Enter a verb: ")
-noun2 = input("Enter the name of a place: ")
-adj = input("Enter an adjective: ")
-adj2 = input("Enter an adjective: ")
-noun3 = input("Enter the name of a place: ")
-verb2 = input("Enter a verb: ")
-noun4 = input("Enter a noun: ")
-verb3 = input("Enter a verb: ")
-noun5 = input("Enter a noun: ")
-adj2 = input("Enter an adjective: ")
-adj3 = input("Enter an adjective: ")
-color = input("Enter a color: ")
-adj4 = input("Enter an adjective: ")
-verb4 = input("Enter a verb: ")
-verb5 = input("Enter a verb: ")
-noun6 = input("Enter a noun: ")
+#color2 = ['purple', 'yellow', 'black']
+noun12 = ['December', 'August', 'October']
+noun13 = input("Enter the name of a month ")
+verb9 = input("Enter a verb: ")
+noun14 = input("Enter the name of a place: ")
+adj6 = input("Enter an adjective: ")
+adj7 = input("Enter an adjective: ")
+noun15 = input("Enter the name of a place: ")
+verb10 = input("Enter a verb: ")
+noun16 = input("Enter a noun: ")
+verb11 = input("Enter a verb: ")
+noun17 = input("Enter a noun: ")
+adj8 = input("Enter an adjective: ")
+adj9 = input("Enter an adjective: ")
+#color3 = input("Enter a color: ")
+adj10 = input("Enter an adjective: ")
+verb12 = input("Enter a verb: ")
+verb13 = input("Enter a verb: ")
+noun18 = input("Enter a noun: ")
 
 # Print story #3
 
@@ -223,23 +242,24 @@ else:
 
 # User inputs story #4
 
-adj = input("Enter an adjective: ")
-noun = input("Enter a noun: ")
-adj2 = input("Enter an adjective: ")
-verb = input("Enter a verb ending in 'ing': ")
-adj3 = input("Enter an adjective: ")
-verb2 = input("Enter a verb ending in 'ing': ")
-adj4 = input("Enter an adjective: ")
-adj5 = input("Enter an adjective: ")
-adj6 = input("Enter an adjective: ")
-verb3 = input("Enter a verb: ")
-adj8 = input("Enter an adjective: ")
-verb4 = input("Enter a verb: ")
-verb5 = input("Enter a verb ending in 'ing': ")
-noun2 = input("Enter a noun: ")
-adj7 = input("Enter an adjective: ")
-adj8 = input("Enter an adjective: ")
-noun3 = input("Enter a noun: ")
+adj11 = ['salty', 'dirty', 'noisy']
+verb14 = ['jump', 'meet', 'send']
+adj12 = input("Enter an adjective: ")
+noun19 = input("Enter a month: ")
+adj12 = input("Enter an adjective: ")
+verb15 = input("Enter a verb ending in 'ing': ")
+adj13 = input("Enter an adjective: ")
+verb16 = input("Enter a verb ending in 'ing': ")
+adj14 = input("Enter an adjective: ")
+adj15 = input("Enter an adjective: ")
+adj16 = input("Enter an adjective: ")
+verb17 = input("Enter a verb: ")
+adj17 = input("Enter an adjective: ")
+verb18 = input("Enter a verb: ")
+verb19 = input("Enter a verb ending in 'ing': ")
+noun20 = input("Enter a noun: ")
+adj18 = input("Enter an adjective: ")
+adj19 = input("Enter an adjective: ")
 
 # print story #4
 
@@ -254,7 +274,7 @@ print("Many people " + verb3 + " on vacation in this weather.")
 time.sleep(3)
 print("It's a " + adj7 + " time to " + verb4 + " things like go " + verb5 + " at a " + noun2 + " or to explore " + adj8 + " places.")
 time.sleep(3)
-print("What's your " + adj8 + " type of " + noun3 + "?")
+print("What's your type of weather?")
 
 answer = input("Let's play, shall we?\n")
 
