0df00a7414d8902594266c770ea833df8305f08d
LucaDP07 <luke07@hotmail.it>
2022-07-22 18:25:07 +0000

    create a function for each variable user input with the instructions to work

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import time
 
-#Function inputs Lists
+# Function inputs Lists
 
 
 def inputs_list(a, b):
-    for i,element in enumerate(a):
+    for i, element in enumerate(a):
         print(i+1, element)
-    chosen=input('')
-    while not chosen.isnumeric() or int(chosen)>len(a):
+    chosen = input('')
+    while not chosen.isnumeric() or int(chosen) > len(a):
         chosen = input("Invalid data: Select" + b)
-    el=a[int(chosen)-1]
+    el = a[int(chosen)-1]
     return el
 
-#Function to start the game
+# Function to start the game
 
 
 def start_game():
@@ -20,95 +20,154 @@ def start_game():
     while not any(answer.lower() == f for f in ['yes', 'y', "no", "n"]):
         answer = input('Please enter yes or no')
     if any(answer.lower() == f for f in ['yes', 'y']):
-        print("Cool, let's play the game!")
-        
+        print("Cool, let's play the game!")  
     else:
         print("That's fine. See you soon\n")
         quit()
 
 # Welcome message
+
+
+def select_name():
+    name = input("Enter your name: ")
+    while not name.isalpha():
+        name = input("Enter a name: ")
+    return name
+
+
+def select_age():
+    age = input("Enter your age: ")
+    while not age.isnumeric():
+        age = input("Enter your age, should be a number: ")
+    return age
+
+
+def select_adjective():
+    adj = ['perfect', 'rough', 'gentle']
+    print('Select an adjective: ')
+    adjective_first = inputs_list(adj, " an adjective")
+    return adjective_first
+
+
 def select_color():
     colour = ['red', 'blue', 'green']
-    print ('Select a color')
-    colorFirst = inputs_list(colour, "a color")
-    return colorFirst
+    print('Select a color')
+    color_first = inputs_list(colour, "a color")
+    return color_first
+
+
+def select_noun():
+    nouns = ['car', 'money', 'banana']
+    print('Select a noun: ')
+    noun_first = inputs_list(nouns, "a noun")
+    return noun_first
+
+
+def select_verb():
+    verbs = ['running', 'starving', 'loving']
+    print('Select a verb: ')
+    verb_first = inputs_list(verbs, "a verb")
+    return verb_first
+
+
+def select_clothing():
+    clothings = ['sock', 'pajama', 'skirt']
+    print('Select your clothing')
+    clothings_first = inputs_list(clothings, "a clothing")
+    return clothings_first
+
+
+def select_adjective_two():
+    adj_second = ['angry', 'able', 'busy']
+    print('Select an adjective: ')
+    adjectives_second = inputs_list(adj_second, "an adjective")
+    return adjectives_second
+
+
+def select_celebrity():
+    celeb = ['Ryan Reynolds', 'Emma Stone', 'Tom Hanks']
+    print('Select a celebrity: ')
+    celebrities = inputs_list(celeb, "a celebrity")
+    return celebrities
+
+
+def select_number():
+    number = input('Enter a number: ')
+    while not number.isnumeric(): 
+        number = input('Enter a number, should be a number: ')
+    return number
+
+
+def select_friend():
+    friend = input('Enter the name of a friend: ')
+    while not friend.isalpha():
+        friend = input('Invalida data: Enter a name: ')
+    return friend
+
+
+def select_noun_two():
+    nouns_second = ['wood', 'ukelele', 'smoothie']
+    print('Select a noun: ')
+    noun_second = inputs_list(nouns_second, "a noun")
+    return noun_second
+
+
+def select_friend_two():
+    friend_second = input('Enter the name of another friend: ')
+    while not friend_second.isalpha():
+        friend_second = input('Invalida data: Enter a name: ')
+        return friend_second
 
     
 def run_game():
-
-
     print("Welcome to the Mad Libs Game")
     time.sleep(2) 
 
-    #Calls the Question Function   
+    # Calls the Question Function   
     
     start_game()               
     time.sleep(2)
 
-    #User inputs story
+    # User inputs story
 
-    #Name input
-    name = input("Enter your name: ")
-    while not name.isalpha():
-        name= input("Enter a name: ")
+    # Name input
+    name = select_name()
 
-    #Age Input
-    age = input("Enter your age: ")
-    while not age.isnumeric():
-        age = input("Enter your age, should be a number: ")
+    # Age Input
+    age = select_age()
 
-    #Adjectives List
-    adj = ['perfect', 'rough', 'gentle']
-    print('Select an adjective: ')
-    adjectiveFirst = inputs_list(adj, " an adjective")
+    # Adjectives List
+    adjective_first = select_adjective
 
-    #Colors List
-    colorFirst = select_color()
+    # Colors List
+    color_first = select_color()
 
-    #Nouns List
-    nouns= ['car', 'money', 'banana']
-    print('Select a noun: ')
-    nounFirst = inputs_list(nouns, "a noun")
+    # Nouns List
+    noun_first = select_noun()
 
-    #Verbs list
-    verbs= ['running', 'starving', 'loving']
-    print('Select a verb: ')
-    verbFirst = inputs_list(verbs, "a verb")
+    # Verbs list
+    verb_first = select_verb()
 
-    #Clothings List
-    clothings = ['sock', 'pajama', 'skirt']
-    print ('Select your clothing')
-    clothingsFirst = inputs_list(clothings, "a clothing")
+    # Clothings List
+    clothings_first = select_clothing()
 
-    #Adjectives Second List
-    adjSecond= ['angry', 'able', 'busy']
-    print('Select an adjective: ')
-    adjectivesSecond = inputs_list(adjSecond, "an adjective")
+    # Adjectives Second List
+    adjectives_second = select_adjective_two()
 
-    #Celebrities List
-    celeb= ['Ryan Reynolds', 'Emma Stone', 'Tom Hanks']
-    print('Select a celebrity: ')
-    celebrities = inputs_list(celeb, "a celebrity")
+    # Celebrities List
+    celebrities = select_celebrity()
 
-    #Number Input
-    number = input('Enter a number: ')
-    while not number.isnumeric(): 
-        number = input('Enter a number, should be a number: ')
+    # Number Input
+    number = select_number()
 
-    #Friend First Input
-    friend = input('Enter the name of a friend: ')
-    while not friend.isalpha():
-        friend = input('Invalida data: Enter a name: ')
+    # Friend First Input
+    friend = select_friend()
 
-    #Nouns Second List
-    nounsSecond = ['wood', 'ukelele', 'smoothie']
-    print('Select a noun: ')
-    nounSecond = inputs_list(nounsSecond, "a noun")
+    # Nouns Second List
+    noun_second = select_noun_two()
 
-    #Friend Second Input
-    friendSecond = input('Enter the name of another friend: ')
-    while not friendSecond.isalpha():
-        friendSecond = input('Invalida data: Enter a name: ')
+    # Friend Second Input
+    friend_second = select_friend_two()
 
     time.sleep(1)
 
@@ -118,26 +177,26 @@ def run_game():
     time.sleep(3)
     print("My name is " + name + " and I'm " + age + " years old.")
     time.sleep(3)
-    print("If I were president, I'd do a whole bunch of " + adjectiveFirst + " things:")
+    print("If I were president, I'd do a whole bunch of " + adjective_first + " things:")
     time.sleep(3)
-    print("I would drive the biggest " + colorFirst + " car in the country, and that")
+    print("I would drive the biggest " + color_first + " car in the country, and that")
     time.sleep(3)
-    print("car would go faster than any other " + nounFirst + " in the world!")
+    print("car would go faster than any other " + noun_first + " in the world!")
     time.sleep(3)
     print("Everyone would eat pepperoni pizza for dinner.")
     time.sleep(3)
-    print("I would live in the Statue of Liberty and build a " + verbFirst + " pool at her feet.")
+    print("I would live in the Statue of Liberty and build a " + verb_first + " pool at her feet.")
     time.sleep(3)
-    print("I would wear a " + clothingsFirst + " on my head, and everyone would say I look " + adjectivesSecond + " like " + celebrities + ".")
+    print("I would wear a " + clothings_first + " on my head, and everyone would say I look " + adjectives_second + " like " + celebrities + ".")
     time.sleep(3)
     print("School would be open only " + number + " days a year.")
     time.sleep(3)
     print("I would give my friends the best jobs.")
     time.sleep(3)
-    print("I would nominate " + friend + " to be secretary of the " + nounSecond + ",")
+    print("I would nominate " + friend + " to be secretary of the " + noun_second + ",")
     time.sleep(3)
-    print("and " + friendSecond + " could be vice president !")
+    print("and " + friend_second + " could be vice president !")
     time.sleep(3)
 
-if __name__ == '__main__':
-    run_game()
+    if __name__ == '__main__':
+        run_game()
